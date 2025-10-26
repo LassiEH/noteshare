3f5b9058d6df026625391e0383a1f6b6198e3106
laelhaar <laelhaar@jyu.fi>
2025-10-26 19:59:49 +0200

    Uses the API and config change to use local azure func address

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import Note from "./models/Note"
-import { api } from "../api/api"
 import NoteList from "./components/NoteList"
 
 //import { mockApi } from './mocks/mockNotes'
@@ -12,18 +11,28 @@ const App = () => {
   const [newNote, setNewNote] = useState("");
 
   const fetchNotes = async () => {
-    const data = await api.getNotes();
+    const res = await fetch("/api/notes");
+    if (!res.ok) throw new Error("Failed fetching the notes");
+    const data = await res.json()
     setNotes(data);
   };
 
   const deleteNote = async (id: string) => {
-    await api.deleteNote(id);
+    const res = await fetch(`/api/notes?id=${id}`, {
+      method: "DELETE"
+    });
+    if (!res.ok) throw new Error("Failed to delete note");
     fetchNotes();
   };
 
   const addNote = async () => {
     if (!newNote.trim()) return;
-    await api.addNote(newNote);
+    const res = await fetch("/api/notes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({ content: newNote })
+    })
+    if (!res.ok) throw new Error("Failed to add note")
     setNewNote("");
     fetchNotes();
   };
